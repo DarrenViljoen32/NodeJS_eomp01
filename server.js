89c35d55380656dcdc098baae388f35b22d6b8f5
DarrenViljoen32 <31759385+DarrenViljoen32@users.noreply.github.com>
2024-03-01 17:08:24 +0200

    Update server.js

--- server.js
+++ server.js
@@ -12,75 +12,60 @@ const PORT = process.env.PORT
 const app = express();
 
 app.use(cors({
-    origin: 'http://localhost:8080',
+    origin: 'http://localhost:8081',
     credentials:true
 }))
 app.use(express.json())
-app.use(express.static('./views'))
-
-app.use(cookieParser())
-
-
+app.use(express.static('../frontend/src/views/ProductsView.vue'))
+app.use(express.static('../frontend/src/views/ItemView.vue'))
+// app.use(cookieParser())
 
 
 
-app.get('/products/:id', (req, res) => {
-    const prodID = req.params;
-    res.json(products);
-  });
-
+// const authenticate = (req,res,next) =>{
+//     let {cookie} = req.headers
+//     let tokenInHeader = cookie && cookie.split('=')[1]
+//     if(tokenInHeader===null) res.sendStatus(401)
+//     jwt.verify(tokenInHeader,process.env.SECRET_KEY,(err,user)=>{
+//     if(err) return res.sendStatus(403)
+//     req.user = user
+//     next()
+// })
+// }
 
 app.use('/users',usersRouter)
 app.use('/products',productsRouter)
 
-// const password = userPass
-app.post('/users',async (req,res)=>{
-    const {userPass} = req.body
-    const hash = await bcrypt.hash(userPass,10)
-
-    
-    res.send({
-        msg: "You have registered successfully"
-     })
-    
-})
-    app.post('/login',(req,res)=>{
-    
+app.post('/login',(req,res)=>{
     res.json({
         token:token,
         msg:'You have logged in'
     })
 })
-    
-})
-
-
 app.post('/users',(req,res)=>{
     const {firstName, lastName, userAge, Gender, userRole, emailAdd, userPass, userProfile} = req.body
-    bcrypt.hash(userPass,10, async (err,hash)=>{
+    bcrypt.hash(password,10, async (err,hash)=>{
         if(err) throw err
-        await addUser(emailAdd,hash)
-        
-        res.send(await addUser(emailAdd,hash))
+        await addUser(username,hash)
+        res.send(await addUser(username,hash))
     })
     res.send({
        msg: "You have registered successfully"
     })
 })
-
-const auth = async (req,res,next) => { 
-    const {firstName, lastName, userAge, Gender, userRole, emailAdd, userPass, userProfile} = req.body
+const auth = async (req,res,next) => {  //middleware (req,res,next())
+    const {password,username} = req.body
     const hashedPassword = await checkUser(username )
-    bcrypt.compare(userPass,hashedPassword, (err,result)=>{
+    bcrypt.compare(password,hashedPassword, (err,result)=>{
         if(err) throw err
         if(result === true){
             const {username} = req.body
         const token = jwt.sign({username:username},
         process.env.SECRET_KEY,{expiresIn:'1h'})
-        res.cookie('jwt',token,{httpOnly:false})
+        res.cookie('jwt',token,{httpOnly:false}) //true only backend user can access on frontend--- if it is set to true than only the backend user can set the
         res.send({
-            token:token,
-            msg:'You are logged in'
+            token:token, //first one is the token ,second one is the value of the token
+            msg:'You have logged in!!! YAY!'
         })
         next()
         }else{
@@ -88,16 +73,9 @@ const auth = async (req,res,next) => {
         }
     })
     }
-
     app.post('/login',auth, (req,res)=>{
         res.send({
-            msg:'You are logged in'
-        })
-    })
-    app.delete('/logout',(req,res)=>{
-        res.clearCookie('jwt')
-        res.send({
-            msg:'logging out'
+            msg:'You have logged in!!! YAY!'
         })
     })
 
