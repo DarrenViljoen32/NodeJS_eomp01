2b3ec9ce5a4cd4f9a499de1d59e87888245e4081
DarrenViljoen32 <31759385+DarrenViljoen32@users.noreply.github.com>
2024-03-01 17:20:07 +0200

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -3,8 +3,8 @@ import axios from 'axios'
 
 // axios.defaults.withCredentials = true;
 
-const baseUrl = 'https://nodejs-eomp-vi7f.onrender.com'
-// const baseUrl = 'http://localhost:8090'
+// const baseUrl = 'https://nodejs-eomp-vi7f.onrender.com'
+const baseUrl = 'http://localhost:8090'
 
 
 function ProductDetails(props) {
